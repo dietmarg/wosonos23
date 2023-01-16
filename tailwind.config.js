9c913b60cf56f7a5a4b43c9c477b17cc3fe225f0
Dietmar <dietmar.gigler@reinblau.de>
2023-01-16 17:15:38 +0100

    front page prototype

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,9 @@ module.exports = {
     },
   },
   variants: {},
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio')
+  ],
 };
